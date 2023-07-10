5892939abe5da2683c4b4762f88bf906a1e9012e
Chakradhar2003 <95245987+Chakradhar2003@users.noreply.github.com>
2023-07-10 18:03:57 +0530

    Update requestMethods.jsx

--- client/src/requestMethods.jsx
+++ client/src/requestMethods.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "https://atlas-one-alpha.vercel.app/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YTJhYzdhZTBhMGFhZGE2ZWM3MmNlMyIsImlzQWRtaW4";
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
